09c9290edc1e75f4ea1f77b4f4eb6d6e5d413fbf
Allison Karlitskaya <allison.karlitskaya@redhat.com>
2024-04-29 10:37:32 +0200

    Install ruff from distro packages
    add rope to pylsp, install pyright.

--- pipx.sh
+++ pipx.sh
@@ -3,8 +3,6 @@ pipx uninstall-all
 pipx install autoimport
 
 pipx install python-lsp-server
-pipx inject python-lsp-server pylsp-mypy pylint
+pipx inject python-lsp-server pylsp-mypy pylint pylsp-rope
 
-pipx install ruff
-
-pipx install ruff-lsp
+pipx install pyright
